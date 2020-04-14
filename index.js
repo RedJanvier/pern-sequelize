b7db8cf5bf9b452a3c7d6bc2784b201748afb5d3
RedJanvier <janvierntwali@gmail.com>
2020-04-14 22:48:00 +0200

    chore(server): moved all server to root folder

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
-
-const server = require('./server');
+const express = require('express');
+const routes = require('../routes');
 
 const PORT = process.env.PORT || 3300;
 
-server.listen(PORT, () => console.log(`Server is live at localhost: ${ PORT }`))
+const server = express();
+server.use(express.json());
+
+server.use('/api', routes);
+
+server.listen(PORT, console.log(`Server started on http://localhost:${PORT}/api`))
+
+module.exports = server;
